7820efa6f37098f3ee04d02ea288b89eb5cf0c72
Nick Graham <nick.graham@jointheleague.org>
2023-11-24 17:28:34 -0800

    Fixed movement

--- src/com/alvontaymaher/GamePanel.java
+++ src/com/alvontaymaher/GamePanel.java
@@ -26,18 +26,18 @@ public class GamePanel extends JPanel implements KeyListener {
 	Random r = new Random();
 	JFrame frame = new JFrame("2048");
 	int[] empty = new int[2];
-	
+
 	public GamePanel() {
 
 		frame.add(this);
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
-	//	frame.setBackground(new Color(50,50,70));	
+		//	frame.setBackground(new Color(50,50,70));	
 		frame.setVisible(true);
 		frame.addKeyListener(this);
 		frame.setDefaultCloseOperation(3);
 		this.setBackground(Color.white);
-		
-		
+
+
 		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < 4; j++) {
 
@@ -46,10 +46,10 @@ public class GamePanel extends JPanel implements KeyListener {
 
 			}
 		}
-		
+
 		newTile();
 		newTile();
-		
+
 		frame.pack();
 		repaint();
 
@@ -62,7 +62,7 @@ public class GamePanel extends JPanel implements KeyListener {
 		}
 
 	}
-	
+
 	private boolean anyEmpty() {
 		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < 4; j++) {
@@ -73,116 +73,208 @@ public class GamePanel extends JPanel implements KeyListener {
 		}
 		return false;
 	}
-	
+
 	private int generateStartingValue() {
 		return r.nextInt(4) == 0? 4: 2;
 	}
-	
+
 	private int[] findEmptyTile() {
-		
+
 		int row, col;
-		
+
 		do {
 			row = r.nextInt(4);
 			col = r.nextInt(4);
 		} while (tiles[row][col].getValue() != 0);
-		
+
 		int[] values = {row, col};
-			
+
 		return values;
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) { 
 
-		
-		
+
+
 		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < 4; j++) {
 				try {
 					tiles[j][i].draw(g);
 				}catch(Exception e) {
-				System.out.println(j + ", " + i);
+					System.out.println(j + ", " + i);
 				}
 			}
 		}
-		
+
 
 	}
 
 	private void move(int direction) {
 
-		
-		
-//		if space (direction) is empty move there
-//		if space (direction) is another tile of same value then combine
-		
+
+
+		//		if space (direction) is empty move there
+		//		if space (direction) is another tile of same value then combine
+
 		int r, c;
-		
-//		if up start with row at 0, if down start with row at 3 ect
-//		switch(direction) {
-//			case UP: row = 1;
-//			break;
-//			case DOWN: row = 3;
-//			break;
-//			case LEFT: col = 1;
-//			break;
-//			case RIGHT: col = 3;
-//			break;
-//		}
-		
-		for(r = 0; r < 4; r++) {
-			for(c = 0; c < 4; c++) {
-				if (tiles[r][c].getValue() != 0) {
-					moveUp(r,c);
+
+		//		if up start with row at 0, if down start with row at 3 ect
+		//		switch(direction) {
+		//			case UP: row = 1;
+		//			break;
+		//			case DOWN: row = 3;
+		//			break;
+		//			case LEFT: col = 1;
+		//			break;
+		//			case RIGHT: col = 3;
+		//			break;
+		//		}
+		boolean newTileNeeded = false;
+		if(direction == UP) {
+
+			for(r = 1; r < 4; r++) {
+				for(c = 0; c < 4; c++) {
+					if (tiles[r][c].getValue() != 0) {
+						if(moveUp(r,c)) {
+							newTileNeeded = true;
+						}
 					}
 				}
-				
+
 			}
+		}
+		if(direction == DOWN) {
+			for(r = 3; r > -1; r--) {
+				for(c = 0; c < 4; c++) {
+					if (tiles[r][c].getValue() != 0) {
+						if(moveDown(r,c)) {
+							newTileNeeded = true;
+						}
+					}
+				}
+
+			}
+		}
+		if(direction == LEFT) {
+			for(c = 1; c < 4; c++) {
+				for(r = 0; r < 4; r++) {
+					if (tiles[r][c].getValue() != 0) {
+						if(moveLeft(r,c)) {
+							newTileNeeded = true;
+						}
+					}
+				}
+
+			}
+		}
+		if(direction == RIGHT) {
+			for(c = 3; c > -1; c--) {
+				for(r = 0; r < 4; r++) {
+					if (tiles[r][c].getValue() != 0) {
+						if(moveRight(r,c)) {
+							newTileNeeded = true;
+						}
+					}
+				}
+
+			}
+		}
+		
+		if(newTileNeeded) {
+			newTile();
+		}
 	}
 
-	private void moveUp(int r, int c) {
+
+	private boolean moveUp(int r, int c) {
 		int row = r;
+
+		boolean returnVal = false;
 		
 		while (row > 0 && tiles[row-1][c].getValue() == 0) {
 			row--;
 		}
-		
+
 		tiles[row][c].setValue(tiles[r][c].getValue());
-		tiles[r][c].setValue(0);
-		
+		if(row!=r) {
+			tiles[r][c].setValue(0);
+			returnVal = true;
+		}
 		if (row > 0 && tiles[row-1][c].getValue() == tiles[row][c].getValue()) {
 			tiles[row-1][c].setValue(tiles[row][c].getValue()*2);
 			tiles[row][c].setValue(0);
+			returnVal = true;
 		}
-		
+		return returnVal;
+
 	}
-	
-	private void moveDown(int r, int c) {
+
+	private boolean moveDown(int r, int c) {
 		int row = r;
+
+		boolean returnVal = false;
 		
-		while (row > 0 && tiles[row-1][c].getValue() == 0) {
-			row--;
+		while (row <3 && tiles[row+1][c].getValue() == 0) {
+			row++;
 		}
-		
+
 		tiles[row][c].setValue(tiles[r][c].getValue());
-		tiles[r][c].setValue(0);
-		
-		if (row > 0 && tiles[row-1][c].getValue() == tiles[row][c].getValue()) {
-			tiles[row-1][c].setValue(tiles[row][c].getValue()*2);
+		if(row!=r) {
+			tiles[r][c].setValue(0);
+			returnVal = true;
+		}
+		if (row <3  && tiles[row+1][c].getValue() == tiles[row][c].getValue()) {
+			tiles[row+1][c].setValue(tiles[row][c].getValue()*2);
 			tiles[row][c].setValue(0);
+			returnVal = true;
+		}
+		return returnVal;
+	}
+
+	private boolean moveRight(int r, int c) {
+		int col = c;
+		boolean returnVal = false;
+		
+		while (col < 3 && tiles[r][col+1].getValue() == 0) {
+			col++;
+		}
+
+		tiles[r][col].setValue(tiles[r][c].getValue());
+		if(col != c) {
+			tiles[r][c].setValue(0);
+			returnVal = true;
+		}
+		if (col < 3  && tiles[r][col+1].getValue() == tiles[r][col].getValue()) {
+			tiles[r][col+1].setValue(tiles[r][col].getValue()*2);
+			tiles[r][col].setValue(0);
+			returnVal = true;
 		}
 		
+		return returnVal;
 	}
-	
-	private void moveRight() {
+
+	private boolean moveLeft(int r, int c) {
+		int col = c;
+		boolean returnVal = false;
 		
+		while (col > 0 && tiles[r][col-1].getValue() == 0) {
+			col--;
+		}
+
+		tiles[r][col].setValue(tiles[r][c].getValue());
+		if(col != c) {
+			tiles[r][c].setValue(0);
+			returnVal = true;
+		}
+		if (col > 0  && tiles[r][col-1].getValue() == tiles[r][col].getValue()) {
+			tiles[r][col-1].setValue(tiles[r][col].getValue()*2);
+			tiles[r][col].setValue(0);
+			returnVal = true;
+		}
+		return returnVal;
 	}
-	
-	private void moveLeft() {
-		
-	}
-	
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -215,8 +307,8 @@ public class GamePanel extends JPanel implements KeyListener {
 		if (key == KeyEvent.VK_RIGHT) {
 			move(RIGHT);
 		}
-		
-//		newTile();
+
+	
 		repaint();
 
 	}
